146956a8d8dbed260ee7e202cebf73825cfe6504
Deepak Kumar Sarwa <deepaksarwa7@gmail.com>
2023-04-01 21:42:48 +0530

    init

---
+++ main.js
@@ -0,0 +1,18 @@
+let countryField = document.getElementById('countryCode');
+(async function getCountry(){
+  let html=''
+  const data = await fetch('http://localhost:3000/country').then(res=>res.json())
+  console.log('data',data);
+  data.forEach(element => {
+     html+= `<option>+${element.code} ${element.country}</option>`;
+  });
+  countryField.innerHTML= html
+})()
+
+countryField.addEventListener('change',()=>{
+  console.log('change',countryField );
+  let code = countryField.value.split(" ")[0]
+  console.log('code',code);
+  // countryField.innerText = code
+})
+
